14e9594bee52996e641791b8f8dcb904a538cd4c
Anelia <r1031474@student.thomasmore.be>
2026-04-14 14:49:24 +0200

    Add backend MVP auth flow

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// routes
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
+
+// server start
+console.log("...SERVER IS RUNNING ...");
+app.listen(3000);
